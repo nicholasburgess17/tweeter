a47318f4110782d2887104ad956d5d49bc881baa
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-11-15 21:04:03 +0000

    implement character counter on new tweet form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  $("#tweet-text").keyup(function(e) {
+    const maxLength = 140;
+    const strLength = e.target.value.length
+    const charRemaining = (maxLength - strLength)
+  if (charRemaining < 0) {
+    document.querySelector(".counter").classList.add("red-count")
+  }else {
+    document.querySelector(".counter").classList.remove("red-count")
+  }
+document.querySelector(".counter").textContent = charRemaining
+})
+  
 });
