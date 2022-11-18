ad02716a71759fc0fcb7ad28202335b9ecc40f57
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-11-18 22:33:06 +0000

    create loadTweets function and make an ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,8 @@
-// Fake data taken from initial-tweets.json
-
-//Tweet Object
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-// create a JavaScript function that will generate the DOM structure for a tweet, given a tweet object.
+const loadTweets = () => {
+  $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
+    console.log("Success: ", tweets);
+  });
+};
 
 //create tweet element
 const createTweetElement = (data) => {
@@ -57,5 +34,11 @@ const renderTweets = (tweets) => {
     $("#tweets-container").append(render);
   }
 };
-$(document).ready(() => {renderTweets(data)})
-
+$(document).ready(() => {
+  renderTweets(data);
+  $("#target").submit(function (event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+    loadTweets();
+  });
+});
